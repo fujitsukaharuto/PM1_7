9169398eb05ad4d385405c7f9188d957f9866f40
k023g0055@g.neec.ac.jp <k023g0055@g.neec.ac.jp>
2023-11-08 12:41:25 +0900

    上2つやりました

--- main.cpp
+++ main.cpp
@@ -1,12 +1,37 @@
 #include <Novice.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
 
 const char kWindowTitle[] = "LC1A_22_PM";
+
+struct Vector2 {
+	float x;
+	float y;
+};
+
+struct Ball {
+	Vector2 position;
+	float time;
+	Vector2 StartPosition;
+	Vector2 EndPosition;
+	float result;
+	int IsEasing;
+};
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
+
+	Ball ball[4]{};
+
+	for (int i = 0; i < 4; i++) {
+		ball[i].position = { 50.0f,(i + 1) * 100.0f };
+		ball[i].StartPosition = { 50.0f,i*100.0f};
+		ball[i].EndPosition = { 1230.0f,i * 100.0f };
+
+	}
 
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
@@ -25,6 +50,32 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+		for (int i = 0; i < 4; i++) {
+			if (keys[DIK_SPACE] && ball[i].IsEasing == 0) {
+				ball[i].IsEasing = 1;
+			}
+
+			if (ball[i].IsEasing == 1) {
+				if (ball[i].time < 1) {
+					ball[i].time += 0.01f;
+				}
+				if (ball[i].time > 1) {
+					ball[i].time = 1.0f;
+				}
+
+				if (i == 0) {
+					ball[i].position.x = (1 - ball[i].time) * ball[i].StartPosition.x + ball[i].time * ball[i].EndPosition.x;
+				}
+				if (i == 1) {
+					ball[i].result = -(cosf(float(M_PI) * ball[i].time) - 1) / 2;
+					ball[i].position.x = (1 - ball[i].result) * ball[i].StartPosition.x + ball[i].result * ball[i].EndPosition.x;
+				}
+			}
+
+		}
+
+		
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -32,6 +83,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		for (int i = 0; i < 4; i++) {
+			Novice::DrawEllipse((int)ball[i].position.x, (int)ball[i].position.y, 50, 50, 0.0f, RED, kFillModeSolid);
+		}
 
 		///
 		/// ↑描画処理ここまで
